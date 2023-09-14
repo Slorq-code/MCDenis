a3a5599218ee57b339b5abda49477665a88ea7b8
Laura Valeria Navarro <lvna.95@gmail.com>
2023-09-13 20:54:18 -0500

    Redux login

--- Fronted/src/index.js
+++ Fronted/src/index.js
@@ -4,10 +4,15 @@ import './styles/normalize.css';
 import reportWebVitals from './reportWebVitals';
 import AppRouter from './router/router'
 
+import { Provider } from 'react-redux'
+import store from './store/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
      <AppRouter />
+    </Provider>
   </React.StrictMode>
 );
 reportWebVitals();
---
+++ Fronted/src/store/auth/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const authSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    status: 'not-authenticated', //'checking' 'not-authenticated' 'authenticated'
+    uid: null,
+    email: null,
+    displayName: null,
+    photoURL: null,
+    errorMessage: null
+  },
+  reducers: {
+    login: (state, action)=>{},
+    logout: (state, payload)=>{},
+    checkingCredentials: (state)=>{},
+
+
+  },
+})
+
+export const { login, logout, checkingCredentials } = authSlice.actions
+
